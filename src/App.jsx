1ac03f75dfb7239faf8f1052996e65deab91b323
Rodrigo Santos <rodrigo.santos@check24.de>
2020-08-14 12:44:38 +0200

    Adding some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useQuery } from "graphql-hooks";
 import "./App.css";
 
@@ -16,26 +17,67 @@ const QUERY = `
   }
 `;
 
+const RepositoryList = ({ repositoryCollection }) => {
+  return (
+    <div className="RepositoryList">
+      {repositoryCollection.map((r) => (
+        <div className="RepositoryList-item" key={r.name}>
+          <div className="RepositoryList-itemHeader">{r.name}</div>
+          <p className="RepositoryList-itemDescription">
+            {r.description || "No description provided"}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+RepositoryList.propTypes = {
+  repositoryCollection: PropTypes.array,
+};
+
 function App() {
-  const [username, setUsername] = React.useState();
-  const [variables, setVariables] = React.useState();
+  const [username, setUsername] = React.useState("rodrigosaint");
+  const [variables, setVariables] = React.useState({ username });
   const { data, loading } = useQuery(QUERY, { variables });
   const updateUserName = React.useCallback(
     (event) => setUsername(event.target.value),
     []
   );
 
-  const search = React.useCallback(() => setVariables({ username }), [
-    username,
-  ]);
+  const search = React.useCallback(
+    (event) => {
+      setVariables({ username });
+      event.preventDefault();
+    },
+    [username]
+  );
 
   return (
     <div className="App">
-      <input onChange={updateUserName} />
-      <button disabled={!username || loading} onClick={search}>
-        Search
-      </button>
-      {JSON.stringify(data)}
+      <form onSubmit={search}>
+        <label className="App-label" for="username-input">
+          Username
+        </label>
+        <div className="App-inputArea">
+          <input
+            id="username-input"
+            className="App-input"
+            value={username}
+            onChange={updateUserName}
+          />
+          <button
+            className="App-searchButton"
+            disabled={!username || loading}
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      {data && (
+        <RepositoryList repositoryCollection={data.user.repositories.nodes} />
+      )}
     </div>
   );
 }
