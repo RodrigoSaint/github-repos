f9cdb876010bc243f0f4baaf2580b7430222e339
Rodrigo Santos <rodrigosaint@Rodrigos-MBP.fritz.box>
2020-08-16 17:00:21 +0200

    Setting auth token as env variable

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { ClientContext, GraphQLClient } from "graphql-hooks";
 const client = new GraphQLClient({
   url: "https://api.github.com/graphql",
   headers: {
-    authorization: "bearer 10b58b30446b03e30ec2e8e4d471098e5292c64e",
+    authorization: `bearer ${process.env.REACT_APP_GIHUB_TOKEN}`,
   },
 });
 
